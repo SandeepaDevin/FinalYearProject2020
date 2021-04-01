dea1014f2ffd03b7c6d93f0bb8cea1be8c81b504
SandeepaDevin <48104360+SandeepaDevin@users.noreply.github.com>
2021-04-01 23:03:06 +0530

    Create WifiApplication.cc

---
+++ veins_inet/src/veins_inet/WifiApplication.cc
@@ -0,0 +1,77 @@
+#include "veins_inet/WifiApplication.h"
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/common/TagBase_m.h"
+#include "inet/common/TimeTag_m.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+#include "inet/networklayer/common/L3AddressTag_m.h"
+#include "inet/transportlayer/contract/udp/UdpControlInfo_m.h"
+
+#include "veins_inet/VeinsInetSampleMessage_m.h"
+
+using namespace inet;
+
+Define_Module(WifiApplication);
+
+WifiApplication::WifiApplication()
+{
+}
+
+bool WifiApplication::startApplication()
+{
+    // host[0] should stop at t=20s
+    if (getParentModule()->getIndex() == 0) {
+        auto callback = [this]() {
+            getParentModule()->getDisplayString().setTagArg("i", 1, "red");
+
+            traciVehicle->setSpeed(0);
+
+            auto payload = makeShared<VeinsInetSampleMessage>();
+            timestampPayload(payload);
+            payload->setChunkLength(B(100));
+            payload->setRoadId(traciVehicle->getRoadId().c_str());
+
+            auto packet = createPacket("accident");
+            packet->insertAtBack(payload);
+            sendPacket(std::move(packet));
+
+            // host should continue after 30s
+            auto callback = [this]() {
+                traciVehicle->setSpeed(-1);
+            };
+            timerManager.create(veins::TimerSpecification(callback).oneshotIn(SimTime(30, SIMTIME_S)));
+        };
+        timerManager.create(veins::TimerSpecification(callback).oneshotAt(SimTime(20, SIMTIME_S)));
+    }
+
+    return true;
+}
+
+bool WifiApplication::stopApplication()
+{
+    return true;
+}
+
+WifiApplication::~WifiApplication()
+{
+}
+
+void WifiApplication::processPacket(std::shared_ptr<inet::Packet> pk)
+{
+    auto payload = pk->peekAtFront<VeinsInetSampleMessage>();
+
+    EV_INFO << "Received packet: " << payload << endl;
+
+    getParentModule()->getDisplayString().setTagArg("i", 1, "green");
+
+    traciVehicle->changeRoute(payload->getRoadId(), 999.9);
+
+    if (haveForwarded) return;
+
+    auto packet = createPacket("relay");
+    packet->insertAtBack(payload);
+    sendPacket(std::move(packet));
+
+    haveForwarded = true;
+}
